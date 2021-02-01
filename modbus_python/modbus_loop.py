42bccf4d0accd5609f0d7e73714f6ad2f99ce4e9
Danil <macknill2012@gmail.com>
2021-02-01 20:49:56 +0300

    modbus ok

--- modbus_python/modbus_loop.py
+++ modbus_python/modbus_loop.py
@@ -2,46 +2,50 @@ import minimalmodbus
 import serial
 import time
 from itertools import repeat
-import json
 
-print("Start modbus")
 
 class Jetsonmodbus:
-	def __init__(self, Serial):
-		self.errors = 0
+    def __init__(self, Serial):
+        self.errors = 0
         self.freq = 20 #hz
-		self.mb = minimalmodbus.Instrument(Serial,1,minimalmodbus.MODE_RTU)
-		self.mb.serial.baudrate = 115200         # Baud
-		self.mb.serial.bytesize = 8
-		self.mb.serial.parity   = serial.PARITY_NONE
-		self.mb.serial.stopbits = 1
-		self.mb.serial.timeout  = 1          # second
-		self.mb.address = 1
-		self.mb.clear_buffers_before_each_transaction = True
-		self.holdRegisters = list(repeat(0, 29))
-		self.inpRegisters = list(repeat(0, 29))
-		self.open_cap = 0;
-	
-    def set_cap(self, open_cap)
-        self.open_cap = open_cap
-            
+        self.mb = minimalmodbus.Instrument(Serial,1,minimalmodbus.MODE_RTU)
+        self.mb.serial.baudrate = 115200         # Baud
+        self.mb.serial.bytesize = 8
+        self.mb.serial.parity   = serial.PARITY_NONE
+        self.mb.serial.stopbits = 1
+        self.mb.serial.timeout  = 1          # second
+        self.mb.address = 1
+        self.mb.clear_buffers_before_each_transaction = True
+        self.registers_quantity = 30
+        self.reg_write_addr = 0;
+        self.holdRegisters = list(repeat(0, self.registers_quantity))
+        self.inpRegisters = list(repeat(0, self.registers_quantity))
+        self.write_cmd = 0;
+        self.holRegisters = self.mb.read_registers(0, self.registers_quantity, 3)
+        self.queue = []
 
-	def loop(self):
-		self.life = True
-		while self.life:
-			self.registers = self.mb.read_registers(0, 30, 4)
-			try:
-				time.sleep(0.005)
-                write_registers(0, self.holdRegisters)			
-			except:
-				self.errors = self.errors + 1
-                
-			time.sleep(1/self.freq)
+    def write_reg(self, reg_addr, reg_data, need_wait):
+        if reg_addr < self.registers_quantity:
+            task = [int(reg_addr), int(reg_data)]
+            self.queue.append(task)
+            if need_wait:
+                while len(self.queue):
+                    nothing += 1
 
-		
+    def read_reg(self, reg_addr):
+        if reg_addr < self.registers_quantity:
+            return self.inpRegisters[reg_addr]
 
-"""
-if __name__ == "__main__":
-	jetboard = jetsonmodbus("/dev/ttyUSB1")
-	jetboard.loop()
-"""
+    def loop(self):
+        self.life = True
+        while self.life:
+            time.sleep(1/self.freq)
+            self.inpRegisters = self.mb.read_registers(0, self.registers_quantity, 4)
+            if len(self.queue):
+                task = self.queue.pop(0)
+                try:
+                    time.sleep(0.05)
+                    self.mb.write_register(task[0], task[1])
+                except:
+                    self.errors = self.errors + 1
+                    time.sleep(0.05)
--- modbus_python/modbus_work.py
+++ modbus_python/modbus_work.py
@@ -1,4 +1,4 @@
-import modbus
+import modbus_loop
 import threading
 import os
 import time
@@ -6,20 +6,27 @@ import subprocess
 import datetime
 import time
 
-stmboard = modbus.Jetsonmodbus("/dev/ttyUSB0")
+
+stmboard = modbus_loop.Jetsonmodbus("/dev/ttyUSB0")
 
 thread_modbus = threading.Thread(target = stmboard.loop, name = "proc_jetboard")
 thread_modbus.start()
 
 print("start car drive!!!")
+
+inc = 0;
+
 try:
     while True:
-        print (stmboard.read_reg(2))
-        time.sleep(0.5)        
-        print(stmboard.inpRegisters)
-        stmboard.write_reg(3, 160)
-        time.sleep(0.5)        
-         
+        print ("////////////")
+        print ("write: " + str(inc))
+        stmboard.write_reg(0, inc, 0)
+        print ("Reg: " + str(stmboard.read_reg(0)))
+        time.sleep(0.75)
+        if inc < 4:
+            inc += 1
+        else:
+            inc = 0
 
 except KeyboardInterrupt:
     stmboard.life = 0
